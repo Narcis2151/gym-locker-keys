b86dbe3d893695bdcbbd03fd0ea4bfa3d8818044
Narcis Fanica <narcis.fanica@vivacredit.ro>
2025-02-22 21:47:38 +0200

    refactor streamlit app to have 2 tabs, one for each gym

--- app.py
+++ app.py
@@ -7,21 +7,35 @@ from google.cloud import bigquery
 from fetch_data_from_bigquery import fetch_data_from_bigquery
 from generate_heatmap import generate_heatmap
 
-
 def main(bq_client):
     st.title("Gym Locker Assignment Analysis")
 
-    images_data = fetch_data_from_bigquery(bq_client)
-    heatmap_data = generate_heatmap(images_data)
-    plt.figure(figsize=(12, 8))
-    annotations = np.arange(1, 163).reshape(18, 9)
-    sns.heatmap(heatmap_data, annot=annotations, cbar=True, fmt="d")
-    plt.title("Locker Usage Heatmap")
-    plt.xlabel("Locker Number")
-    plt.xticks([])
-    plt.yticks([])
-    st.pyplot(plt)
+    # Fetch data and generate heatmaps
+    images_700_fit, images_sweat = fetch_data_from_bigquery(bq_client)
+    heatmap_data_700_fit = generate_heatmap(images_700_fit)
+    heatmap_data_sweat = generate_heatmap(images_sweat, nr_lockers=160)
 
+    tab1, tab2 = st.tabs(["700 Fit", "Sweat"])
+
+    with tab1:
+        annotations = np.arange(1, 163).reshape(18, 9)
+        plt.figure(figsize=(12, 8))
+        sns.heatmap(heatmap_data_700_fit, annot=annotations, cbar=True, fmt="d")
+        plt.title("Locker Usage Heatmap (700 Fit)")
+        plt.xlabel("Locker Number")
+        plt.xticks([])
+        plt.yticks([])
+        st.pyplot(plt)
+
+    with tab2:
+        annotations2 = np.arange(1, 161).reshape(16, 10)
+        plt.figure(figsize=(12, 8))
+        sns.heatmap(heatmap_data_sweat, annot=annotations2, cbar=True, fmt="d")
+        plt.title("Locker Usage Heatmap (Sweat)")
+        plt.xlabel("Locker Number")
+        plt.xticks([])
+        plt.yticks([])
+        st.pyplot(plt)
 
 if __name__ == "__main__":
     bq_client = bigquery.Client()
--- fetch_data_from_bigquery.py
+++ fetch_data_from_bigquery.py
@@ -1,9 +1,18 @@
-def fetch_data_from_bigquery(bq_client):
+from typing import Tuple
+
+
+def fetch_data_from_bigquery(bq_client) -> Tuple[list, list]:
     query = """
-    SELECT locker_number
+    SELECT locker_number, gym
     FROM `gym-locker-keys.gym_locker_data.image_classification`
     """
     query_job = bq_client.query(query)
     results = query_job.result()
-    locker_numbers = [row.locker_number for row in results]
-    return locker_numbers
+    locker_numbers = {
+        '700_fit': [],
+        'sweat': []
+    }
+    for row in results:
+        locker_numbers[row.gym].append(row.locker_number)
+
+    return locker_numbers['700_fit'], locker_numbers['sweat']
